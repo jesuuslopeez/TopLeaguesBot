228b4f7b8853e0e40f253c5fdb2a8edc472730d4
Jesús López Pérez <jlopezp@iesrafaelalberti.es>
2025-05-06 17:02:13 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import pytz
 
 # Token del bot de Telegram
-TOKEN = "TU_TOKEN_AQUI"
+TOKEN = "YOUR_TOKEN"
 
 # API Key para obtener datos de fútbol
-FDO_API_KEY = "TU_API_KEY_AQUI"
+FDO_API_KEY = "YOUR_API_KEY"
 
 # Zona horaria para España
 SPAIN_TZ = pytz.timezone('Europe/Madrid')
